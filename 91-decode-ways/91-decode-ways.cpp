db02225921c4ef09495f0c8aee65161aa37a760b
Taara Sinh Aatrey <taarasinh1@gmail.com>
2022-01-19 20:29:47 +0530

    Time: 3 ms (40.81%), Space: 6.3 MB (42.68%) - LeetHub

--- 91-decode-ways/91-decode-ways.cpp
+++ 91-decode-ways/91-decode-ways.cpp
@@ -1,33 +1,30 @@
 class Solution {
 public:
     
-    int numDecodings(string s) {
-        // ASSUMING 1 BASED INDEXING IN DP
-        int n = s.size();
-        
-        // dp[i] represents the number of ways to decode the string s[1].s[2]......s[i-1]..s[i]
-        vector<int> dp(n + 1, 0);
-        dp[0] = 1;
-        
-        for (int i = 1; i <= n; i++) {
-            
-            // checking if the current character is not '0' since '0' is not avaiable for mapping
-            if (s[i - 1] != '0')
-            // so i decoded ith character, now let's decode s[1]..s[2]........s[i-2]...s[i-1]
-                dp[i] += dp[i - 1];
-            
-            if (i > 1 && s[i - 2] != '0') {
-                
-                int code = (s[i - 2] - '0') * 10 + s[i - 1] - '0';
-                
-                if (code <= 26)
-                // so i decoded (s[i - 1]...s[i]) substring, now let's decode s[1]..s[2]........s[i-3]...s[i-2]
-                    dp[i] += dp[i - 2];
-                
-            }
-            
+    int rec(string &s, int i, int n, vector<int> &dp) {
+        if (i < 0) {
+            return 1;
         }
-        
-        return dp.back();
+        if (dp[i] != -1) {
+            return dp[i];
+        }
+        dp[i] = 0;
+        int ch = s[i] - '0';
+        if (ch >= 1) {
+            dp[i] += rec(s, i - 1, n, dp);
+        }
+        if (i - 1 >= 0) {
+            ch = (s[i - 1] - '0') * 10 + ch;
+            if (ch >= 10 && ch <= 26) {
+                dp[i] += rec(s, i - 2, n, dp);
+            }
+        }
+        return dp[i];
+    }
+    
+    int numDecodings(string s) {
+        int n = s.size();
+        vector<int> dp(n, -1);
+        return rec(s, n - 1, n, dp);
     }
 };

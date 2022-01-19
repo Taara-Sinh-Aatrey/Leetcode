29d5c0b47cc3d0373f92023b99cf3592dad40dce
Taara Sinh Aatrey <taarasinh1@gmail.com>
2022-01-19 20:50:20 +0530

    Time: 20 ms (16.80%), Space: 16.3 MB (8.42%) - LeetHub

---
+++ 740-delete-and-earn/740-delete-and-earn.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int deleteAndEarn(vector<int>& nums) {
+        const int N = 1e4 + 1;
+        
+        vector<int> freq(N);
+        for (auto &x : nums) {
+            freq[x]++;
+        }
+        
+        // dp[i] represents the money earned when i is the biggest element taken
+        vector<int> dp(N), mx(N);
+        
+        for (int number = 1; number < N; number++) {
+            
+            dp[number] = freq[number] * number;
+            
+            // mx[number] = max(dp[1], dp[2], dp[number])
+            if (number >= 2)
+                dp[number] += mx[number - 2];
+            
+            mx[number] = max(dp[number], mx[number - 1]);
+        }
+        
+        return mx[N - 1];
+    }
+};

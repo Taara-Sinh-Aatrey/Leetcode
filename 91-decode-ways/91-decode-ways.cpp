3221b572a56dc2e615afe6c7a607984372ef32e9
Taara Sinh Aatrey <taarasinh1@gmail.com>
2022-01-19 20:22:01 +0530

    Time: 5 ms (19.49%), Space: 6.3 MB (22.85%) - LeetHub

--- 91-decode-ways/91-decode-ways.cpp
+++ 91-decode-ways/91-decode-ways.cpp
@@ -3,16 +3,17 @@ public:
     
     int numDecodings(string s) {
         int n = s.size();
-        vector<int> dp(n, 0);
-        for (int i = 0; i < n; i++) {
+        vector<int> dp(n + 1, 0);
+        dp[0] = 1;
+        for (int i = 1; i <= n; i++) {
             // i is the end of the current susbtring
-            if (s[i] != '0') {
-                dp[i] += (i - 1 >= 0 ? dp[i - 1] : 1);
+            if (s[i - 1] != '0') {
+                dp[i] += dp[i - 1];
             }
-            if (i > 0 && s[i - 1] != '0') {
-                int code = (s[i - 1] - '0') * 10 + s[i] - '0';
+            if (i > 1 && s[i - 2] != '0') {
+                int code = (s[i - 2] - '0') * 10 + s[i - 1] - '0';
                 if (code <= 26) {
-                    dp[i] += (i - 2 >= 0 ? dp[i - 2] : 1);
+                    dp[i] += dp[i - 2];
                 }
             }
         }

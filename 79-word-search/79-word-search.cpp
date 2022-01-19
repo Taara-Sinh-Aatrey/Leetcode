04d8e83031fbe9ee98322a265b0a7187916eeed1
Taara Sinh Aatrey <taarasinh1@gmail.com>
2022-01-19 18:06:09 +0530

    Time: 826 ms (18.16%), Space: 8.3 MB (5.94%) - LeetHub

---
+++ 79-word-search/79-word-search.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    bool exist(vector<vector<char>>& board, string word) {
+        int n = board.size(), m = board[0].size();
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (board[i][j] != word[0]) {
+                    continue;
+                }
+                
+                vector<vector<bool>> act(n, vector<bool>(m));
+                const int dr[] = {-1, 0, 1, 0};
+                const int dc[] = {0, -1, 0, 1};
+                auto e = [&] (int r, int c) {
+                    return 0 <= r && r < n && 0 <= c && c < m && !act[r][c];
+                };
+                
+                function<bool(int, int, int)> dfs = [&] (int r, int c, int idx) -> bool {
+                    if (board[r][c] != word[idx])
+                        return false;
+                    
+                    if (idx + 1 == word.size())
+                        return true;
+                    
+                    act[r][c] = true;
+                    
+                    for (int dir = 0; dir < 4; dir++) {
+                        int nr = r + dr[dir];
+                        int nc = c + dc[dir];
+                        if (e(nr, nc)) {
+                            if (dfs(nr, nc, idx + 1)) {
+                                return true;
+                            }
+                        }
+                    }
+                    
+                    act[r][c] = false;
+                    return false;
+                };
+                
+                if (dfs(i, j, 0)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+};

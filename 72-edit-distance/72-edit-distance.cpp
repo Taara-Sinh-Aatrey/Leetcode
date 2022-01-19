3ed22e9cac83ebd660a28f3fcc5ad5b3505f48e1
Taara Sinh Aatrey <taarasinh1@gmail.com>
2022-01-19 15:17:19 +0530

    Time: 16 ms (53.82%), Space: 9.1 MB (33.41%) - LeetHub

---
+++ 72-edit-distance/72-edit-distance.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        int n = word1.size(), m = word2.size();
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1));
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= m; j++) {
+                if (i == 0 || j == 0) {
+                    dp[i][j] = max(i, j);
+                }
+                else {
+                    dp[i][j] = min({dp[i - 1][j - 1] + (word1[i - 1] != word2[j - 1]), dp[i - 1][j] + 1, dp[i][j - 1] + 1});
+                }
+            }
+        }
+        return dp[n][m];
+    }
+};

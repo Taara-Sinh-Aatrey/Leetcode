9b28c38a64f5fdcf8703850429413a1259e85ede
Taara Sinh Aatrey <taarasinh1@gmail.com>
2022-01-20 20:21:22 +0530

    Time: 8 ms (7.71%), Space: 6 MB (64.99%) - LeetHub

--- 96-unique-binary-search-trees/96-unique-binary-search-trees.cpp
+++ 96-unique-binary-search-trees/96-unique-binary-search-trees.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
-    int generate(int n, int low, int high, vector<vector<int>> &dp) {
-        if (low > high)
+    int generate(int n, vector<int> &dp) {
+        if (n == 0)
             return 1;
         
-        int &answer = dp[low][high];
+        int &answer = dp[n];
         
         if (answer != -1)
             return answer;
         
         answer = 0;
         
-        for (int x = low; x <= high; x++)
-            answer += generate(n, low, x - 1, dp) * generate(n, x + 1, high, dp);
+        for (int x = 0; x < n; x++)
+            answer += generate(x, dp) * generate(n - 1 - x, dp);
 
         return answer;
     }
     
     int numTrees(int n) {
-        vector<vector<int>> dp(n + 1, vector<int>(n + 1, -1));
+        vector<int> dp(n + 1, -1);
         
-        return generate(n, 1, n, dp);
+        return generate(n, dp);
     }
 };

ac95a03258e308d9ef66b9a9fbd6d14507ca7801
Taara Sinh Aatrey <taarasinh1@gmail.com>
2022-01-20 18:22:20 +0530

    Time: 4 ms (98.33%), Space: 6.5 MB (56.32%) - LeetHub

--- 37-sudoku-solver/37-sudoku-solver.cpp
+++ 37-sudoku-solver/37-sudoku-solver.cpp
@@ -1,42 +1,45 @@
 class Solution {
 public:
-    void solveSudoku(vector<vector<char>>& board) {
-        solve(board);
+    
+    bool solve(vector<vector<char>>& board, vector<vector<bool>> &row, vector<vector<bool>> &col, vector<vector<vector<bool>>> &box, int i, int j) {
+        if (i == 9 && j == 0) {
+            return true;
+        }
+        int ni = i, nj = j + 1;
+        if (j == 9) {
+            ni++;
+            nj = 0;
+        }
+        if (board[i][j] != '.') {
+            return solve(board, row, col, box, ni, nj);
+        }
+        for (int x = 1; x < 10; x++) {
+            if (row[i][x] || col[j][x] || box[i / 3][j / 3][x]) {
+                continue;
+            }
+            row[i][x] = col[j][x] = box[i / 3][j / 3][x] = true;
+            board[i][j] = '0' + x;
+            if (solve(board, row, col, box, ni, nj)) {
+                return true;
+            }
+            board[i][j] = '.';
+            row[i][x] = col[j][x] = box[i / 3][j / 3][x] = false;
+        }
+        return false;
     }
     
-    bool solve(vector<vector<char>>& board){
-        for(int i = 0; i < board.size(); i++){
-            for(int j = 0; j < board[0].size(); j++){
-                if(board[i][j] == '.'){
-                    for(char c = '1'; c <= '9'; c++){
-                        if(isValid(board, i, j, c)){
-                            board[i][j] = c; 
-                            
-                            if(solve(board))
-                                return true; 
-                            else
-                                board[i][j] = '.'; 
-                        }
-                    }
-                    
-                    return false;
-                }
+    void solveSudoku(vector<vector<char>>& board) {
+        vector<vector<bool>> row(9, vector<bool>(10));
+        vector<vector<bool>> col(9, vector<bool>(10));
+        vector<vector<vector<bool>>> box(3, vector<vector<bool>>(3, vector<bool>(10)));
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] == '.')
+                    continue;
+                int x = board[i][j] - '0';
+                row[i][x] = col[j][x] = box[i / 3][j / 3][x] = true;
             }
         }
-        return true;
-    }
-    
-    bool isValid(vector<vector<char>>& board, int row, int col, char c){
-        for(int i = 0; i < 9; i++) {
-            if(board[i][col] == c) 
-                return false; 
-            
-            if(board[row][i] == c) 
-                return false; 
-            
-            if(board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] == c) 
-                return false; 
-        }
-        return true;
+        solve(board, row, col, box, 0, 0);
     }
 };

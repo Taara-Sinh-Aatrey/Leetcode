505a72493213a2bf0d139f2ee861e21dae6f7fcc
Taara Sinh Aatrey <taarasinh1@gmail.com>
2022-01-21 12:34:05 +0530

    Time: 60 ms (28.20%), Space: 6.4 MB (95.59%) - LeetHub

---
+++ 115-distinct-subsequences/115-distinct-subsequences.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int numDistinct(string s, string t) {
+        
+        int n = s.size(), m = t.size();
+        vector<long long> dp(m + 1);
+        dp[0] = 1;
+        
+        const long long inf = 1e18;
+        
+        for (int i = 1; i <= n; i++) {
+            for (int j = m; j > 0; j--) {
+                if (s[i - 1] == t[j - 1]) {
+                    dp[j] += dp[j - 1];
+                    if (dp[j] > inf) dp[j] = inf;
+                }
+            }
+        }
+        
+        return dp[m];
+    }
+};

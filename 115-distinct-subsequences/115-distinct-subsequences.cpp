27000518e3e8b059bc270dbae85e268dcbb5b989
Taara Sinh Aatrey <taarasinh1@gmail.com>
2022-01-21 12:36:36 +0530

    Time: 29 ms (65.58%), Space: 6.4 MB (95.59%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.cpp
+++ 115-distinct-subsequences/115-distinct-subsequences.cpp
@@ -6,13 +6,10 @@ public:
         vector<unsigned long long> dp(m + 1);
         dp[0] = 1;
         
-        // const long long inf = 1e18;
-        
         for (int i = 1; i <= n; i++) {
             for (int j = m; j > 0; j--) {
                 if (s[i - 1] == t[j - 1]) {
                     dp[j] += dp[j - 1];
-                    // if (dp[j] > inf) dp[j] = inf;
                 }
             }
         }

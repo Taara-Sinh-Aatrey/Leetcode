3eb3bc6669dea5ffdeaa398bb4c105783aa96567
Taara Sinh Aatrey <taarasinh1@gmail.com>
2022-01-23 18:20:12 +0530

    Time: 4 ms (36.13%), Space: 7.7 MB (72.25%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -3,18 +3,11 @@ public:
     int rob(vector<int>& a) {
         int n = a.size();
         const int inf = 1e9L + 5;
+        
         vector<int> dp(2, -inf);
-    
-        for (int i = 0; i < n; i++) {
-
-            int prev = dp[0];
-
-            int cur = max({a[i] + prev, a[i], prev});
-
-            cur = max(cur, dp[1]);
-
-            dp = {dp[1], cur};
-        }
+        
+        for (int i = 0; i < n; i++)
+            dp = {dp[1], max({dp[1], a[i] + dp[0], a[i], dp[0]})};
 
         return dp.back();
     }

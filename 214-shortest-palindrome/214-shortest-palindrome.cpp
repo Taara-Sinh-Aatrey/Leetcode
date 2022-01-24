8bb9cff524c26d148863842c2b2d38a05d8cd532
Taara Sinh Aatrey <taarasinh1@gmail.com>
2022-01-24 19:44:18 +0530

    Time: 0 ms (100.00%), Space: 8.1 MB (31.97%) - LeetHub

---
+++ 214-shortest-palindrome/214-shortest-palindrome.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    
+    string shortestPalindrome(string s) {
+        int n = s.size();
+        
+        string rev = s;
+        reverse(rev.begin(), rev.end());
+        s = s + "#" + rev;
+        
+        int len = 0;
+        vector<int> lps(s.size(), 0);
+        for (int i = 1; i < s.size(); i++) {
+            while (len > 0 && s[i] != s[len])
+                len = lps[len - 1];
+            
+            if (s[i] == s[len])
+                len++;
+            
+            lps[i] = len;
+        }
+        
+        cout << lps.back() << endl;
+        
+        return rev.substr(0, n - lps.back()) + s.substr(0, n);
+    }
+};

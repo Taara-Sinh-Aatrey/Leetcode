065a8ebf1dfc8c38564ba3b2dd4a0a356d1b6906
Taara Sinh Aatrey <taarasinh1@gmail.com>
2022-01-24 20:43:54 +0530

    Time: 151 ms (24.98%), Space: 52 MB (28.70%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.cpp
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        unordered_map<int, bool> vis;
+        map<int, bool> vis;
         
         for (auto &x : nums) {
             if (vis[x])

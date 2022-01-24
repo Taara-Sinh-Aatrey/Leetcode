9fcb9b4cd31b237f898ba1a57cf048bd269835ee
Taara Sinh Aatrey <taarasinh1@gmail.com>
2022-01-25 02:11:52 +0530

    Time: 110 ms (10.15%), Space: 20.7 MB (9.79%) - LeetHub

---
+++ 218-the-skyline-problem/218-the-skyline-problem.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    vector<vector<int>> getSkyline(vector<vector<int>>& buildings) {
+        
+        set<int> endPoints;
+        map<int, vector<pair<int, int>>> mp;
+        for (auto &pr : buildings) {
+            endPoints.emplace(pr[0]);
+            endPoints.emplace(pr[1]);
+            mp[pr[0]].emplace_back(pr[1], pr[2]);
+            cout << pr[2] << endl;
+        }
+        
+        set<pair<int, int>> st;
+        
+        vector<vector<int>> ans;
+        
+        for (auto &x : endPoints) {
+            
+            while (!st.empty() && st.begin()->first <= x) {
+                st.erase(st.begin());
+            }
+            
+            for (auto [y, h] : mp[x]) {
+                st.emplace(y, h);
+                auto it = prev(st.lower_bound({y, INT_MAX}));
+                auto jt = it;
+                while (jt != st.begin() && prev(jt)->second <= it->second) {
+                    jt = prev(jt);
+                }
+                if (it != st.end() && next(it)->second >= it->second)
+                    it = next(it);
+                st.erase(jt, it);
+            }
+            
+            ans.push_back({x, st.empty() ? 0 : st.begin()->second});
+            
+        }
+        
+        vector<vector<int>> nans;
+        for (int i = 0; i < ans.size(); i++) {
+            if (nans.empty() || nans.back()[1] != ans[i][1]) {
+                nans.emplace_back(ans[i]);
+            }
+        }
+        
+        return nans;
+    }
+};

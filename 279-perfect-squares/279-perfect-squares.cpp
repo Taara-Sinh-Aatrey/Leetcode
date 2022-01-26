7bf024481cfb35c604311caf16fa3ca878000ae3
Taara Sinh Aatrey <taarasinh1@gmail.com>
2022-01-26 10:05:48 +0530

    Time: 267 ms (34.30%), Space: 9.1 MB (44.03%) - LeetHub

--- 279-perfect-squares/279-perfect-squares.cpp
+++ 279-perfect-squares/279-perfect-squares.cpp
@@ -7,11 +7,9 @@ public:
         for (int i = 0; i * i <= n; i++)
             ans[i * i] = 1;
         
-        for (int i = 0; i < n; i++) {
-            for (int j = 1; i + j * j <= n; j++) {
+        for (int i = 0; i < n; i++)
+            for (int j = 1; i + j * j <= n; j++)
                 ans[i + j * j] = min(ans[i + j * j], ans[i] + 1);
-            }
-        }
         
         return ans[n];
     }

43fbbb278da07f7fe526a54b9604b1a40840913f
Taara Sinh Aatrey <taarasinh1@gmail.com>
2022-01-26 16:32:48 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (72.65%) - LeetHub

--- 343-integer-break/343-integer-break.cpp
+++ 343-integer-break/343-integer-break.cpp
@@ -1,24 +1,12 @@
 class Solution {
 public:
     int integerBreak(int n) {
-    
-        // int ans = 0;
-        // int k = -1;
         
         int m = max(2, (n + 1) / 3);
-            
-            int each = n / m;
-            int rem = n % m;
-            
-            int prod = 1;
-            for (int i = 0; i < m; i++)
-                prod *= each + (i < rem ? 1 : 0);
-            
-            // if (prod > ans)
-            //     k = m;
-            // ans = max(ans, prod);
-        
-        // cout << k << endl;
+
+        int prod = 1;
+        for (int i = 0; i < m; i++)
+            prod *= n / m + (i < (n % m) ? 1 : 0);
         
         return prod;
     }
